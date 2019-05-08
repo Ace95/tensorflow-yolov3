76ef7226e4e54d4cf4d03237dfbab5257c2b202d
Marco Acerbis <marco.acerbis95@gmail.com>
2019-05-08 17:40:44 +0200

    dataset generation updated

---
+++ dataset_gen.py
@@ -0,0 +1,74 @@
+#######################################################################
+#### Author: Marco Acerbis, 4Solid s.r.l. #############################
+#### Copyright (C) 2019 * Ltd. All rights reserved. ###################
+#### File: Datatset_gen.py ############################################
+#### Date: 27/03/19 ###################################################
+#### Editor: Visual Studio Code #######################################
+#######################################################################
+
+import cv2
+import numpy as np
+import io
+
+file = open("dataset.txt",'a')
+
+# CALLBACK FUNCTION
+drawing = False # True if mouse is pressed
+ix,iy = -1,-1
+
+def draw_rectangle(event,x,y,flags,param):
+    global ix,iy,drawing,mode
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        
+        # When you click DOWN with left mouse button drawing is set to True
+        drawing = True
+        # Then we take note of where that mouse was located
+        ix,iy = x,y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        # Once you lift the mouse button, drawing is False
+        drawing = False
+        # we complete the rectangle.
+        cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),3)
+        line = [" ",str(ix)," ",str(iy)," ",str(x)," ",str(y)," ","0"]
+        file.writelines(line)
+
+# GENERATE THE FILELIST
+""" file2 = open("filelist.txt","a")
+for i in range(1,201):
+
+    line = ["\n","./s3_dataset/images/fire-",str(i),".bmp"]
+    file2.writelines(line)
+
+file2.close() """
+
+f = open("./filelist.txt")
+  
+lines = [line.rstrip('\n') for line in f.readlines()]
+
+
+for path in lines:
+    
+    # This names the window so we can reference it 
+    cv2.namedWindow(winname='fire')
+
+    # Connects the mouse button to our callback function
+    cv2.setMouseCallback('fire',draw_rectangle)
+
+    img = cv2.imread(path)
+    line = ["\n",path]
+    file.writelines(line)
+
+    while True: 
+
+        # Shows the image window
+        cv2.imshow('fire',img)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cv2.destroyAllWindows()
+  
+  
+file.close()
